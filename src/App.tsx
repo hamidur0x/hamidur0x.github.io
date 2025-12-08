92b75428b85791d86eeec5a6e4a92ff6053e1dbd
Md Hamidur Rahman <hamidurrahman0705@gmail.com>
2025-12-08 21:40:12 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import Index from "../pages/Index";
-import About from "../pages/About";
-import Projects from "../pages/Projects";
-import Contact from "../pages/Contact";
-import NotFound from "../pages/NotFound";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
